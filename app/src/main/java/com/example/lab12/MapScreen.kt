5b4299461ffd18163ed3876301a6018ad603f8ff
Jeanpiero Zea <jeanpiero.zea@tecsup.edu.pe>
2024-10-21 19:02:13 -0500

    Añadir mas ubicaciones y poligonos

--- app/src/main/java/com/example/lab12/MapScreen.kt
+++ app/src/main/java/com/example/lab12/MapScreen.kt
@@ -34,6 +34,27 @@ fun MapScreen() {
         LatLng(-16.4205151, -71.4945209), // Paucarpata
         LatLng(-16.3524187, -71.5675994) // Zamacola
     )
+    // Coordenadas para los polígonos
+    val mallAventuraPolygon = listOf(
+        LatLng(-16.432292, -71.509145),
+        LatLng(-16.432757, -71.509626),
+        LatLng(-16.433013, -71.509310),
+        LatLng(-16.432566, -71.508853)
+    )
+
+    val parqueLambramaniPolygon = listOf(
+        LatLng(-16.422704, -71.530830),
+        LatLng(-16.422920, -71.531340),
+        LatLng(-16.423264, -71.531110),
+        LatLng(-16.423050, -71.530600)
+    )
+
+    val plazaDeArmasPolygon = listOf(
+        LatLng(-16.398866, -71.536961),
+        LatLng(-16.398744, -71.536529),
+        LatLng(-16.399178, -71.536289),
+        LatLng(-16.399299, -71.536721)
+    )
 
 
 
@@ -52,6 +73,25 @@ fun MapScreen() {
                     snippet = "Punto de interés"
                 )
             }
+            // Dibujar polígonos en el mapa
+            Polygon(
+                points = plazaDeArmasPolygon,
+                strokeColor = Color.Red,
+                fillColor = Color.Blue,
+                strokeWidth = 5f
+            )
+            Polygon(
+                points = parqueLambramaniPolygon,
+                strokeColor = Color.Red,
+                fillColor = Color.Blue,
+                strokeWidth = 5f
+            )
+            Polygon(
+                points = mallAventuraPolygon,
+                strokeColor = Color.Red,
+                fillColor = Color.Blue,
+                strokeWidth = 5f
+            )
         }
         LaunchedEffect(Unit) {
             cameraPositionState.animate(
@@ -59,5 +99,6 @@ fun MapScreen() {
                 durationMs = 3000
             )
         }
+
     }
 }
